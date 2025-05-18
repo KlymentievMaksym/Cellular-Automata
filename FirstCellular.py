89ef72966ecea0b20c1ea9dc33db234533fcdc94
Dadmon <maksklym-ipt27@lll.kpi.ua>
2025-05-18 23:03:12 +0300

    Some things works

--- FirstCellular.py
+++ FirstCellular.py
@@ -34,33 +34,42 @@ def GetNeigbour(position: np.ndarray, indexes: np.ndarray, full_shape: tuple):
 #     return neigbours
 
 
-size = 1000
-array = np.random.randint(0, 2, size=(size,))
-# print(np.unique_counts(array))
-dim = len(array.shape)
-index = np.array([-1, 0, 1])
-# indexes = np.meshgrid(*list(index for _ in range(dim)))
-print(array[None, :])
-plt.axis('off')
-automata = plt.imshow(array[None, :], cmap="cool")
-# plt.show()
+size = 100
+array = np.random.randint(0, 51, size=(size, size))
+array = np.random.randint(0, 51, size=(1280, 1920))
 
 
-def rules(array, x):
-    if array[x - 1] == 1 and array[x] == 1 and array[(x + 1) % array.shape[0]] == 1:
-        array[x] = 0
-    elif array[x - 1] == 0 and array[x] == 0 and array[(x + 1) % array.shape[0]] == 0:
-        array[x] = 0
-    elif array[x - 1] == 1 and array[x] == 0 and array[(x + 1) % array.shape[0]] == 0:
-        array[x] = 0
+def rules(array, x, y):
+    x_00 = array[x][y]
+    if x_00 > 0:
+        array[x][y] -= 1
     else:
-        array[x] = 1
+        array[x][y] = 50
+
+# def rules(array, x, y):
+#     x_10 = array[x - 2][y-1]
+#     x_11 = array[x - 1][y-1]
+#     x_12 = array[x][y-1]
+#     if x_10 == 1 and x_11 == 1 and x_12 == 1:
+#         array[x - 1][y-1] = 0
+#     elif x_10 == 0 and x_11 == 0 and x_12 == 0:
+#         array[x - 1][y-1] = 0
+#     elif x_10 == 1 and x_11 == 0 and x_12 == 0:
+#         array[x - 1][y-1] = 0
+#     else:
+#         array[x - 1][y-1] = 1
 
 
 iterations = 100
 save = []
 
-for iteration in tqdm(
+
+def rules_vectorized(arr):
+    return np.where(arr > 0, arr - 1, 50)
+
+
+# Основний цикл з векторизованими правилами
+for _ in tqdm(
         range(iterations),
         desc="Processing",
         unit="step",
@@ -68,57 +77,9 @@ for iteration in tqdm(
         colour='cyan',
         total=iterations
 ):
-    array_to_change = array.copy()
-    for index_x in range(array.shape[0]):
-        rules(array_to_change, index_x)
-    array = array_to_change.copy()
-    save.append(array)
-
-
-def update(frame):
-    plt.title(f"Iter {frame}")
-    automata.set_data(save[frame][None, :])
-    return automata
-
-
-anim = animation.FuncAnimation(fig=plt.gcf(), func=update, frames=len(save), interval=100, repeat=True)
-# anim.save(f"Hi{time()}.gif", fps=30)
-plt.show()
-
-# def count_on(array, x, y, what_count):
-#     # counts = 0
-#     # counts = np.unique_counts(array[*GetNeigbour([x, y], indexes, array.shape)])
-#     neigbours = array[*GetNeigbour([x, y], indexes, array.shape)]
-#     counts = np.unique_counts(neigbours)
-#     # for neighbour in neighbours:
-#     #     if neighbour > 1:
-#     #         count += 1
-#     # print(counts.counts[what_count])
-#     return counts._asdict().get(what_count, 0)
-
-
-# def rule1(array, x, y):
-#     # count_on(array, x, y, 1) > 2
-#     if array[x, y] == 0 and count_on(array, x, y, 2) == 2:
-#         array[x, y] = 2
-#     elif array[x, y] == 2:
-#         array[x, y] = 1
-#     elif array[x, y] == 1:
-#         array[x, y] = 0
-
-
-# rules = [rule1]
-# iterations = 10
-# save = []
-
-# # rule1(array, 1, 1)
-
-# plt.axis('off')
-# automata = plt.imshow(array, "cool")
-# color = plt.gcf().colorbar(automata)
-# states = np.max(array) - np.min(array)
-# color.set_ticks(range(0, states + 1))
-# color.set_ticklabels([str(i) for i in range(0, states + 1)])
+    array = rules_vectorized(array)
+    save.append(array.copy())
+# rules_vec = np.vectorize(rules, excluded=[0])
 
 # for iteration in tqdm(
 #         range(iterations),
@@ -129,140 +90,35 @@ plt.show()
 #         total=iterations
 # ):
 #     array_to_change = array.copy()
+#     # index_x = np.arange(array.shape[0], step=1)
+#     # index_y = np.arange(array.shape[1], step=1)
+#     # index_x, index_y = np.meshgrid(index_x, index_y)
 #     for index_x in range(array.shape[0]):
 #         for index_y in range(array.shape[1]):
-#             for rule in rules:
-#                 rule(array_to_change, index_x, index_y)
+#             rules(array_to_change, index_x, index_y)
 #     array = array_to_change.copy()
-#     save.append(array)
+#     save.append(array.copy())
 
-
-# def update(frame):
-#     # plt.title(f"Iter {frame}")
-#     automata.set_data(save[frame])
-#     return automata
-
-
-# anim = animation.FuncAnimation(fig=plt.gcf(), func=update, frames=len(save), interval=300, repeat=True)
-# # anim.save(f"Hi{time()}.gif", fps=30)
-# plt.show()
-
-
-# neigbours_index_array = np.zeros((size, size, 2, 8))
-# array = np.zeros((size,) * 2)
-# print(neigbours_index_array)
-# array = [[Cell() for _ in range(size)] for _ in range(size)]
-# arr = [[0 for _ in range(size)] for _ in range(size)]
-# dim_x = len(array)
-# dim_y = len(array[0])
-# shape = (dim_x, dim_y)
-
-# for index_x in range(dim_x):
-#     for index_y in range(dim_y):
-#         center = (index_x, index_y)
-#         array[index_x][index_y].ConnectNeighbours(GetNeigbour(center, array, shape))
-#         array[index_x][index_y].state = np.random.randint(0, 3)
-#         arr[index_x][index_y] = array[index_x][index_y].state
-
-
-# def count_on(neighbours: list):
-#     count = 0
-#     for neighbour in neighbours:
-#         if neighbour > 1:
-#             count += 1
-#     return count
-
-
-# def rule1(cell: Cell):
-#     if cell.state == 0 and count_on(cell.neighbours) > 1:
-#         cell.state = 2
-#     elif cell.state == 2:
-#         cell.state = 1
-#     elif cell.state == 1:
-#         cell.state = 0
-
-
-# rules = [rule1]
-# iterations = 300
-# save = []
-# for iteration in tqdm(
-#         range(iterations),
-#         desc="Processing",
-#         unit="step",
-#         bar_format="{l_bar}{bar:40}{r_bar}",
-#         colour='cyan',
-#         total=iterations
-# ):
-#     for cell_row in array:
-#         for cell in cell_row:
-#             # print(cell)
-#             for rule in rules:
-#                 cell.ApplyRule(rule)
-#     # print(array)
-#     for index_x in range(dim_x):
-#         for index_y in range(dim_y):
-#             arr[index_x][index_y] = array[index_x][index_y].state
-#     # print(arr)
-#     save.append(copy.deepcopy(arr))
-#     # print(save)
-
+fig = plt.figure(figsize=(19.2, 10.8), dpi=100)
+# fig.set_size_inches(1080, 1920)
+# fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
+# ax = plt.Axes(fig, [0., 0., 1., 1.])
+ax = plt.axes([0., 0., 1., 1.])
+ax.set_axis_off()
+# fig.add_axes(ax)
 # plt.axis('off')
-# automata = plt.imshow(arr, "cool")
-# # color = plt.gcf().colorbar(automata)
-# # color.set_ticks(range(0, 2 + 1))
-# # color.set_ticklabels([str(i) for i in range(0, 2 + 1)])
-
-
-# def update(frame):
-#     # plt.title(f"Iter {frame}")
-#     automata.set_data(save[frame])
-#     return automata
-
-
-# anim = animation.FuncAnimation(fig=plt.gcf(), func=update, frames=len(save), interval=300, repeat=True)
-# anim.save(f"Hi{time()}.gif", fps=30)
-# plt.show()
-
-#     plt.title(f"Iter {iteration}")
-#     color = plt.imshow(arr, cmap="cool")
-#     plt.pause(0.01)
-# color = plt.gcf().colorbar(color)
-# color.set_ticks(range(0, 2 + 1))
-# color.set_ticklabels([str(i) for i in range(0, 2 + 1)])
-# plt.show()
-
-# array = np.empty((40, 40), dtype=object)
-# for i in range(array.shape[0]):
-#     for j in range(array.shape[1]):
-#         array[i, j] = Cell()
-# dim = len(array.shape)
-
-# index = np.array([-1, 0, 1])
-# indexes = np.meshgrid(*list(index for _ in range(dim)))
-
-# for arr_index_x in range(array.shape[0]):
-#     for arr_index_y in range(array.shape[1]):
-#         center = (arr_index_x, arr_index_y)
-#         neigbours_indexes = GetNeigbour(center, indexes, array.shape)
-#         neigbours = array[*neigbours_indexes]
-#         array[*center].ConnectNeighbours(neigbours)
-#         array[*center].state = np.random.randint(-1, 2, 1)
-# print(*center)
-# print(array[*center])
-
-# print()
-# print(array[1, 1])
-# print(array[1, 2].neighbours)
-# print(type(array[1, 1]))
-# print(array)
-# print()
-# array_show = np.array(array, dtype=float)
-# plt.imshow(array_show)
+automata = ax.imshow(array, cmap="binary", aspect='auto')
 # plt.show()
 
 
-# def rule1(cell: Cell):
-#     if np.sum(cell.neighbours) > 2:
-#         cell.state = 0
-#     else:
-#         cell.state = 1
+def update(frame):
+    # plt.title(f"Iter {frame}")
+    automata.set_data(save[frame])
+    return automata
+
+
+# fig = plt.gcf()
+anim = animation.FuncAnimation(fig=fig, func=update, frames=len(save), interval=100)
+# plt.tight_layout(pad=0, rect=(0, 0, 0, 0))
+anim.save(f"./Images/Hi{time()}.gif", dpi=100, fps=20, savefig_kwargs={"bbox_inches":'tight',"transparent":True, "pad_inches":0})
+# plt.show()
